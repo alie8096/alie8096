1401790392676fc773cced5f978887be7d293993
Ali Ebrahimian 001 <ali.e.8096@outlook.com>
2024-10-11 21:51:28 +0330

    Crate basic templete and make home page

---
+++ script.js
@@ -0,0 +1,31 @@
+function toggleDarkMode() {
+    const body = document.body;
+    const darkModeToggle = document.getElementById('dark-mode-toggle');
+
+    // تغییر حالت دارک مود
+    body.classList.toggle('dark-mode');
+
+    // تغییر رنگ دکمه با کلاس active
+    darkModeToggle.classList.toggle('active');
+}
+
+// اسلایدشو
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+const slideInterval = setInterval(nextSlide, 5000);
+
+function nextSlide() {
+    slides[currentSlide].style.display = 'none';
+    currentSlide = (currentSlide + 1) % slides.length;
+    slides[currentSlide].style.display = 'block';
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    slides[0].style.display = 'block';
+});
+
+// منوی نوبار
+function toggleMenu() {
+    const navLinks = document.getElementById('nav-links');
+    navLinks.classList.toggle('show');
+}
